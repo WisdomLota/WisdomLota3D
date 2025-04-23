0aeec907e5ee18d7ff82b9cc15a6593cf2975988
WisdomLota <ngaloru2019@gmail.com>
2025-04-23 11:26:37 +0300

    Rendered my 3D Room JSX from GLB file and testing various lights on it

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { useMediaQuery } from 'react-responsive'
+import { Room } from './Room'
+import HeroLights from './HeroLights'
 
 const HeroExperience = () => {
 
@@ -9,8 +11,8 @@ const HeroExperience = () => {
     const isMobile = useMediaQuery({query: '(max-width: 768px'});
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45}}>
-        <ambientLight intensity={0.2} color="#1a1a40" />
-        <directionalLight position={[5, 5, 5]} intensity={5} />
+
+        
 
         <OrbitControls 
         enablePan={false}
@@ -21,10 +23,16 @@ const HeroExperience = () => {
         maxPolarAngle={Math.PI / 2}
         />
 
-        <mesh>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color="teal" />
-        </mesh>
+        <HeroLights />
+
+        <group
+        scale={isMobile ? 0.7 : 1}
+        position={[0, -3.5, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+        >
+            <Room />
+        </group>
+
     </Canvas>
   )
 }
