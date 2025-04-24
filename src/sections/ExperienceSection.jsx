670d96f25a1be17dbfc767b360c0a5cbd070f629
WisdomLota <ngaloru2019@gmail.com>
2025-04-24 13:07:19 +0300

    Create the TechStack section and TechIcon models and properly rendered them accordingly

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -60,7 +60,7 @@ const ExperienceSection = () => {
         <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
             <div className='w-full h-full md:px-20 px-5'>
                 
-                <TitleHeader sub="My Careeer Overview" title="Professional Work Experience"/>
+                <TitleHeader sub="💻My Careeer Overview" title="Professional Work Experience"/>
 
                 <div className='mt-32 relative'>
                     <div className='relative z-50 xl:space-y-32 space-y-10'>
