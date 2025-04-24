9eac352bb4557ee02ad1203bd53fd6e4cf6cdd9a
WisdomLota <ngaloru2019@gmail.com>
2025-04-24 09:27:24 +0300

    Implemented the right part to show my professional experience and responsibilities, used GSAP to add sleek animations too

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { expCards } from '../constants/index.js'
 import GlowCard from '../components/GlowCard.jsx'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 
 const ExperienceSection = () => {
+
+    useGSAP(() => {
+        gsap.utils.toArray('.timeline-card').forEach((card) => {
+            gsap.from(card, {
+                xPercent: -100,
+                opacity: 0,
+                transformOrigin: 'left left',
+                duration: 1,
+                ease: 'power2.inOut',
+                scrollTrigger: {
+                    trigger: card,
+                    start: 'top 80%'
+                }
+            })
+        })
+
+        gsap.to('.timeline', {
+            transformOrigin: 'bottom bottom',
+        })
+    })
+
     return (
         <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
             <div className='w-full h-full md:px-20 px-5'>
@@ -21,6 +47,30 @@ const ExperienceSection = () => {
                                             <img src={card.imgPath} alt={card.title} />
                                         </div>
                                     </GlowCard>
+                                </div>
+
+                                <div className='xl:w-4/6'>
+                                    <div className='flex items-start'>
+                                        <div className='timeline-wrapper'>
+                                            <div className='timeline'/>
+                                            <div className='gradient-line w-2 h-full border-4'/>
+                                        </div>
+                                        <div className='expText flex xl:gap-20 md:gap-10 gap-5 relative z-20'>
+                                            <div className='timeline-logo'>
+                                                <img src={card.logoPath} alt="logo" />
+                                            </div>
+                                            <div>
+                                                <h1 className='font-semibold text-3xl'>{card.title}</h1>
+                                                <p className='my-5 text-white-50'>📅{card.date}</p>
+                                                <p className='text-[#839cb5] italic'>Responsibilities</p>
+                                                <ul className='list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50'>
+                                                    {card.responsibilities.map((responsibility) => (
+                                                        <li key={responsibility} className='text-lg'>{responsibility}</li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         ))}
