f6f4567a8acf580c806c5d676574bb398550cbc0
WisdomLota <ngaloru2019@gmail.com>
2025-04-24 11:53:18 +0300

    Properly animated the mid section and the right part of my ExperienceSection

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
-import { expCards } from '../constants/index.js'
+import { expCards } from '../constants'
 import GlowCard from '../components/GlowCard.jsx'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
@@ -28,8 +28,33 @@ const ExperienceSection = () => {
 
         gsap.to('.timeline', {
             transformOrigin: 'bottom bottom',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '.timeline',
+                start: 'top center',
+                end: '70% center',
+                onUpdate: (self) => {
+                    gsap.to('.timeline', {
+                        scaleY: 1 - self.progress,
+                    })
+                }
+            }
         })
-    })
+
+        gsap.utils.toArray('.expText').forEach((text) => {
+            gsap.from(text, {
+                xPercent: 0,
+                opacity: 0,
+                duration: 1,
+                ease: 'power2.inOut',
+                scrollTrigger: {
+                    trigger: text,
+                    start: 'top 60%'
+                }
+            })
+        })
+        
+    }, []);
 
     return (
         <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
@@ -53,7 +78,7 @@ const ExperienceSection = () => {
                                     <div className='flex items-start'>
                                         <div className='timeline-wrapper'>
                                             <div className='timeline'/>
-                                            <div className='gradient-line w-2 h-full border-4'/>
+                                            <div className='gradient-line w-1 h-full'/>
                                         </div>
                                         <div className='expText flex xl:gap-20 md:gap-10 gap-5 relative z-20'>
                                             <div className='timeline-logo'>
@@ -61,7 +86,7 @@ const ExperienceSection = () => {
                                             </div>
                                             <div>
                                                 <h1 className='font-semibold text-3xl'>{card.title}</h1>
-                                                <p className='my-5 text-white-50'>📅{card.date}</p>
+                                                <p className='my-5 text-white-50'>&nbsp;📅{card.date}</p>
                                                 <p className='text-[#839cb5] italic'>Responsibilities</p>
                                                 <ul className='list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50'>
                                                     {card.responsibilities.map((responsibility) => (
